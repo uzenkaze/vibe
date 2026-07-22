d15357db0787543e50017d40310bb6ae607ebb88
uzenkaze <uzenkaze@users.noreply.github.com>
2026-07-22 10:56:40 +0900

    fix: Dashboard.jsx import 구문에 useEffect 누락으로 인한 Uncaught ReferenceError 해결

--- carrep/src/pages/Dashboard.jsx
+++ carrep/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import styles from './Dashboard.module.css'
 import { getMaintenanceStatus } from '../utils/carMaintenance'
 import MaintenanceRegisterModal from '../components/MaintenanceRegisterModal'
